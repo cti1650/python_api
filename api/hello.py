a39e85160cbe1cfc0a8ffffe5d1ff2eb4d724eb7
cti1650 <cti1650@gmail.com>
2021-06-21 13:09:02 +0000

    日本語に修正

--- api/hello.py
+++ api/hello.py
@@ -6,7 +6,7 @@ import random
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    json_str=json.dumps({"可能中獎號碼": random.sample(range(10),3)}, ensure_ascii=False)
+    json_str=json.dumps({"ランダム値": random.sample(range(10),3)}, ensure_ascii=False)
     self.send_response(200)
     self.send_header('Content-type', 'application/json')
     self.end_headers()
